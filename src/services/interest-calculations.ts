c03c19b25ec228193a1949e6f9708fb9eba85edb
Josefu <joemilsom@gmail.com>
2025-07-22 07:43:38 +1000

    Add compound interest calculations and tests
    Added error condition for compounding period being less than 0 as this
    does not make sense in the context of the equation.

--- src/services/interest-calculations.ts
+++ src/services/interest-calculations.ts
@@ -1,4 +1,11 @@
 export class InvalidTime extends Error {}
+export class InvalidCompoundFrequency extends Error {}
+
+export enum CompoundFrequency {
+  Monthly = 12,
+  Quarterly = 4,
+  Annually = 1,
+}
 
 export function interestAtMaturity(
   principal: number,
@@ -12,5 +19,29 @@ export function interestAtMaturity(
   // Formula: P(1 + rt)
   const years = timeInMonths / 12;
   const finalBalance = principal * (1 + (annualInterest / 100) * years);
+  return Math.round(finalBalance);
+}
+
+export function calculateCompoundInterest(
+  principal: number,
+  annualInterest: number,
+  timeInMonths: number,
+  compoundFrequency: number
+) {
+  if (timeInMonths < 0) {
+    throw new InvalidTime("Time period for interest calculation must be >= 0");
+  }
+
+  if (compoundFrequency < 0) {
+    throw new InvalidCompoundFrequency("Compound frequency must be >= 0");
+  }
+
+  // Equation: P(1+ r/n)^tn
+  const years = timeInMonths / 12;
+  const finalBalance =
+    principal *
+    (1 + annualInterest / 100 / compoundFrequency) **
+      (years * compoundFrequency);
+
   return Math.round(finalBalance);
 }
